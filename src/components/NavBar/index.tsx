e3fb0ed9535cc8ed6493f2083e9620a7281db2ab
Melksedeque <freelancer@melksedeque.com.br>
2025-05-26 13:09:29 -0300

    Importar componente de Logo no NavBar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,9 +1,10 @@
+import Logo from 'components/Logo'
 import styles from './NavBar.module.css'
 
 export default function NavBar() {
     return (
         <nav className={styles.nav}>
-            <h1>NavBar</h1>
+            <Logo />
         </nav>
     )
 }
