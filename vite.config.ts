a28a7a8d457ae94f91a1283c29579f18dffcd626
Melksedeque <freelancer@melksedeque.com.br>
2025-05-26 13:29:38 -0300

    Configuração de aliases de diretórios e pre processamento de SASS de breakpoints

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      assets: path.resolve(__dirname, './src/assets'),
+      components: path.resolve(__dirname, './src/components'),
+      data: path.resolve(__dirname, './src/data'),
+      pages: path.resolve(__dirname, './src/pages'),
+      styles: path.resolve(__dirname, './src/styles'),
+      types: path.resolve(__dirname, './src/types'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "styles/_breakpoints.scss" as *;
+        `,
+      },
+    },
+  },
 })
