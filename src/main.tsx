1a89292555cad08998a998f95ede2933c8486cb2
Melksedeque <freelancer@melksedeque.com.br>
2025-05-26 11:29:26 -0300

    Chamada de componente navbar no main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import NavBar from './components/NavBar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className='helloWorld'>
-      Hello World! 🖖
-      <small><em>Live long and prosper</em></small>
-    </div>
+    <NavBar />
   </StrictMode>,
 )
