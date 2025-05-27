6844c9051d91983af2040d2192096d572c9a9ce6
Melksedeque <freelancer@melksedeque.com.br>
2025-05-27 12:52:09 -0300

    Adição de componente de busca

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,6 +2,7 @@ import Logo from 'components/Logo'
 import styles from './NavBar.module.scss'
 import classNames from 'classnames'
 import { RiShoppingCart2Line, RiShoppingCartFill } from 'react-icons/ri'
+import Busca from 'components/Busca'
 
 const iconeProps = {
     color: 'white',
@@ -19,7 +20,9 @@ export default function NavBar() {
                     })}>Página Inicial</a>
                 </div>
             </div>
-            <div className={styles.busca}></div>
+            <div className={styles.busca}>
+                <Busca />
+            </div>
             <div className={styles.icones}>
                 <a href="/carrinho">
                     {window.location.pathname === '/carrinho' ? <RiShoppingCartFill {...iconeProps} /> : <RiShoppingCart2Line {...iconeProps} /> }
