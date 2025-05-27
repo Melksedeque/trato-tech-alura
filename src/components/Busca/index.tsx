170ed5f02a496b3595207e169fc0ea9639fb6f92
Melksedeque <freelancer@melksedeque.com.br>
2025-05-27 12:47:10 -0300

    Criação de componente de busca

---
+++ src/components/Busca/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Busca.module.scss'
+
+export default function Busca() {
+    return (
+        <div>
+            <input type="text" placeholder="Busca" />
+            <button>Buscar</button>
+        </div>
+    );
+}
