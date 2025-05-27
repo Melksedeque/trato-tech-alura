889800b726d7de8691e9424a20dc2720bf0a2c19
Melksedeque <freelancer@melksedeque.com.br>
2025-05-27 16:09:38 -0300

    Correção nas classes CSS do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,11 +4,11 @@ import HeaderProps from 'types/Header';
 export default function Header({ titulo, descricao, imagem, className = '' }: HeaderProps) {
     return (
         <header className={`${styles.header} ${className}`}>
-            <div className={styles['header-texto']}>
+            <div className={styles.texto}>
                 <h1>{titulo}</h1>
                 <h2>{descricao}</h2>
             </div>
-            <div className={styles['header-imagem']}>
+            <div className={styles.imagem}>
                 <img src={imagem} alt={titulo} />
             </div>
         </header>
