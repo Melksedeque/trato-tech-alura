9ea5b042926d7aea0e2b260a07efe6eb2080732c
Melksedeque <freelancer@melksedeque.com.br>
2025-05-27 13:09:58 -0300

    Rota de Página Padrão para agrupar as páginas do projeto

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,13 @@
+import PaginaPadrao from "components/PaginaPadrao";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<PaginaPadrao />}>
+                    <Route index element={<div><h1>Hello World!</h1></div>} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
