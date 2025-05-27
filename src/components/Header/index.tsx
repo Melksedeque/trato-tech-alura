0f128c881395d1584f55172482dd852c1a93b0b0
Melksedeque <freelancer@melksedeque.com.br>
2025-05-27 15:34:47 -0300

    Inclusão de Navbar no Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,10 @@
+import NavBar from 'components/NavBar';
 import styles from './Header.module.scss';
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <img src="/logo.png" alt="Logo do AluraFlix" />
-            <form>
-                <input type="text" placeholder="Pesquisar" />
-                <button type="submit">Pesquisar</button>
-            </form>
+            <NavBar />
         </header>
     );
 }
