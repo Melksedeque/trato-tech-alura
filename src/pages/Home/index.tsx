227da5bbf2117d46166879210250b093e71392ee
Melksedeque <freelancer@melksedeque.com.br>
2025-05-27 14:21:21 -0300

    Criação de componente de Página Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Home.module.scss'
+
+export default function Home() {
+    return (
+        <div className={styles.home}>
+            <h1>Home Page do Projeto</h1>
+            <h2>Hello World!</h2>
+        </div>
+    )
+}
