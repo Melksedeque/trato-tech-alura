dbecf1c0a1122693574fb682371fe72b7c0c2de6
Melksedeque <freelancer@melksedeque.com.br>
2025-05-27 13:15:08 -0300

    Criação de rodapé do projeto

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,23 @@
+import styles from './Footer.module.scss'
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
+
+const iconeProps = {
+    color: 'white',
+    size: 24,
+}
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div>
+                <FaFacebook { ...iconeProps } />
+                <FaTwitter { ...iconeProps } />
+                <FaInstagram { ...iconeProps } />
+            </div>
+            <div>
+                <p>Desenhado por <a href="http://www.alura.com.br/" target="_blank" rel="noopener noreferrer">Alura</a></p>
+                <p>Desenvolvido por <a href="http://github.com/Melksedeque" target="_blank" rel="noopener noreferrer">Melksedeque</a></p>
+            </div>
+        </footer>
+    );
+}
