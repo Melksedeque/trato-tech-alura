454224e57690510d4b82c51fb37b5421e8dd2ff2
Melksedeque <freelancer@melksedeque.com.br>
2025-05-28 20:50:55 -0300

    Componente de página de Categorias

--- src/pages/Categoria/index.tsx
+++ src/pages/Categoria/index.tsx
@@ -1,12 +1,20 @@
+import styles from './Categoria.module.scss'
 import { RootState } from 'store/index';
 import Header from "components/Header";
 import { useSelector } from "react-redux";
+import { useParams } from 'react-router-dom';
 
 export default function Categoria() {
-    const categoria = useSelector((state: RootState) => state.categorias);
+    const { nomeCategoria } = useParams();
+    const categoria = useSelector((state: RootState) => state.categorias.find(categoria => categoria.id === nomeCategoria));
     return (
         <div>
-            <Header titulo="" descricao="" imagem={} className={} />
+            <Header
+                titulo={categoria?.nome || ''}
+                descricao={categoria?.descricao || ''}
+                imagem={categoria?.header || ''}
+                className={styles.categorias}
+            />
         </div>
     )
 }
