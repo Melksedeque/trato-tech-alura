3ad73537e8f51b6d9cf84346eac00be0c55fb17c
Melksedeque <freelancer@melksedeque.com.br>
2025-05-28 20:28:49 -0300

    Correção na importação de RootState de store para o mapeamento das categorias

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,9 +2,12 @@ import Header from 'components/Header'
 import styles from './Home.module.scss'
 import imgInicial from 'assets/images/inicial.png'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/index';
 
 export default function Home() {
     const navigate = useNavigate();
+    const categorias = useSelector((state: RootState) => state.categorias)
 
     return (
         <div>
@@ -19,12 +22,12 @@ export default function Home() {
                     <h1>Categorias</h1>
                 </div>
                 <div className={styles.container}>
-                    {categorias.map((categoria, index) => {
-                        <div key={index} onClick={navigate(`/categoria/${categoria.id}`)}>
-                            <img src={categoria.image} alt={categoria.nome} />
+                    {categorias.map((categoria, index) => (
+                        <div key={index} onClick={() => navigate(`/categoria/${categoria.id}`)}>
+                            <img src={categoria.thumbnail} alt={categoria.nome} />
                             <h3>{categoria.nome}</h3>
                         </div>
-                    })}
+                    ))}
                 </div>
             </div>
         </div>
