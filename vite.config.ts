b4f0abda433635c6826bfd98e8725b69168ebc9d
Melksedeque <freelancer@melksedeque.com.br>
2025-05-28 20:28:08 -0300

    Configuração de pasta store na lista de pastas com caminho para importação absoluta

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
       components: path.resolve(__dirname, './src/components'),
       data: path.resolve(__dirname, './src/data'),
       pages: path.resolve(__dirname, './src/pages'),
+      store: path.resolve(__dirname, './src/store'),
       styles: path.resolve(__dirname, './src/styles'),
       types: path.resolve(__dirname, './src/types'),
     },
