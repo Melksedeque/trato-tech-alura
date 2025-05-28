2b2731450b7b398438cf3d56f8d8a64e0b3758a9
Melksedeque <freelancer@melksedeque.com.br>
2025-05-28 20:08:25 -0300

    Passagem de Provider de store para o Main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './routes'
+import store from './store/index'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </StrictMode>,
 )
