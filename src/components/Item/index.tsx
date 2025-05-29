f190b721f30bd10b2be703e73ea2bc0fb5042d97
Melksedeque <freelancer@melksedeque.com.br>
2025-05-29 12:28:16 -0300

    Inicio do componente de Item para importar nas categorias

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Item.module.scss'
+
+export default function Item() {
+    return (
+        <div className={styles.item}>
+            <img src="" alt="" />
+            <h3></h3>
+            <p></p>
+        </div>
+    )
+}
