bd75cf00f43afdd35b8bd9085641ba29631a8edb
Melksedeque <freelancer@melksedeque.com.br>
2025-05-29 13:26:04 -0300

    Função para lidar com mudança de favorito usando o Dispatch do Redux

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -2,6 +2,8 @@ import type { Item } from 'types/Item'
 import styles from './Item.module.scss'
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
 import { FaCartPlus } from 'react-icons/fa'
+import { mudarFavorito } from 'store/reducers/itens'
+import { useDispatch } from 'react-redux'
 
 const iconeProps = {
     size: 24,
@@ -9,12 +11,18 @@ const iconeProps = {
 }
 
 export default function Item(props: Item) {
-    const { titulo, descricao, preco, foto, favorito } = props
+    const { titulo, descricao, preco, foto, favorito, id } = props
     console.log(foto)
     const precoFormatado = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL',
     }).format(preco)
+    const dispatch = useDispatch()
+    
+    function resolverFavorito() {
+        dispatch(mudarFavorito(id))
+    }
+    
     return (
         <div className={styles.item}>
             <div className={styles['item-imagem']}>
@@ -31,8 +39,8 @@ export default function Item(props: Item) {
                     </div>
                     <div className={styles['item-acoes']}>
                         {favorito
-                            ? <AiFillHeart {...iconeProps} color="#FF0000" className={styles['item-acao']} />
-                            : <AiOutlineHeart {...iconeProps} className={styles['item-acao']} />
+                            ? <AiFillHeart {...iconeProps} color="#FF0000" className={styles['item-acao']} onClick={resolverFavorito} />
+                            : <AiOutlineHeart {...iconeProps} className={styles['item-acao']} onClick={resolverFavorito} />
                         }
                         <FaCartPlus {...iconeProps} color={false ? '#1875E8' : iconeProps.color} className={styles['item-acao']} />
                     </div>
