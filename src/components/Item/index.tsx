7a01aa023b9c8493243384db45a235c2648c2033
Melksedeque <freelancer@melksedeque.com.br>
2025-05-29 12:46:10 -0300

    Estrutura do componente de Item

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,13 +1,39 @@
 import { Item } from 'types/Item'
 import styles from './Item.module.scss'
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
+
+const iconeProps = {
+    size: 24,
+    color: '#041833',
+}
 
 export default function Item(props: Item) {
+    const { titulo, descricao, preco, imagem, favorito } = props
+    // console.log(imagem)
+    const precoFormatado = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    }).format(preco)
     return (
         <div className={styles.item}>
-            <img src={props.thumbnail} alt={props.nome} />
-            <h3>{props.nome}</h3>
-            <p>{props.descricao}</p>
-            <p>{props.preco}</p>
+            <div className={styles['item-imagem']}>
+                <img src={imagem} alt={titulo} />
+            </div>
+            <div className={styles['item-descricao']}>
+                <div className={styles['item-titulo']}>
+                    <h3>{titulo}</h3>
+                    <p>{descricao}</p>
+                </div>
+                <div className={styles['item-info']}>
+                    <p>{precoFormatado}</p>
+                </div>
+                <div className={styles['item-acoes']}>
+                    <span>{favorito
+                        ? <AiFillHeart {...iconeProps} color="#FF0000" className={styles['item-acao']} />
+                        : <AiOutlineHeart {...iconeProps} className={styles['item-acao']} />
+                    }</span>
+                </div>
+            </div>
         </div>
     )
 }
