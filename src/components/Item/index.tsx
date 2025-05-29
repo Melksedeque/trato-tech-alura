0f23d8c37a00b8173304ab1b38de01730b0366d1
Melksedeque <freelancer@melksedeque.com.br>
2025-05-29 12:45:41 -0300

    Criação de componente de Item

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,11 +1,13 @@
+import { Item } from 'types/Item'
 import styles from './Item.module.scss'
 
-export default function Item() {
+export default function Item(props: Item) {
     return (
         <div className={styles.item}>
-            <img src="" alt="" />
-            <h3></h3>
-            <p></p>
+            <img src={props.thumbnail} alt={props.nome} />
+            <h3>{props.nome}</h3>
+            <p>{props.descricao}</p>
+            <p>{props.preco}</p>
         </div>
     )
 }
