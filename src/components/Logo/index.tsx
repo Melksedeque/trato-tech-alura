92b512cdf0eff934280f60800fba34140314ea67
Melksedeque <freelancer@melksedeque.com.br>
2025-05-30 14:04:31 -0300

    Substituição de tags <a> por componente <Link> do react router dom

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom';
 import styles from './Logo.module.scss';
 import logo from 'assets/images/logo.svg';
 
 export default function Logo() {
     return (
         <div className={styles.logo}>
-            <a href="/">
+            <Link to="/">
                 <img src={logo} alt="Logo Trato Tech" />
-            </a>
+            </Link>
         </div>
     )
 }
