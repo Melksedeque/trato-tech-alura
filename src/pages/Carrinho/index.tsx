5539e85ff34234495179e3569b8cae3c0582bb19
Melksedeque <freelancer@melksedeque.com.br>
2025-05-30 15:30:08 -0300

    Criação de componente de página de Carrinho

---
+++ src/pages/Carrinho/index.tsx
@@ -0,0 +1,39 @@
+import Header from 'components/Header'
+import Item from 'components/Item'
+import { ItemCarrinho } from 'types/Item'
+import { CartItem } from 'types/Carrinho'
+import styles from './Carrinho.module.scss'
+import { RootState } from 'store/index'
+import { useSelector } from 'react-redux'
+
+export default function Carrinho() {
+    const carrinho = useSelector((state: RootState) => {
+        const carrinhoReduce = state.carrinho.reduce((itens: ItemCarrinho[], itemNoCarrinho: CartItem) => {
+            const item = state.itens.find(item => item.id === itemNoCarrinho.id);
+            if (item) {
+                itens.push({
+                    ...item,
+                    quantidade: itemNoCarrinho.quantidade,
+                });
+            }
+            return itens;
+        }, [] as ItemCarrinho[]);
+        return carrinhoReduce;
+    })
+    return (
+        <>
+            <Header
+                titulo='Carrinho de compras'
+                descricao='Confira os produtos que você adicionou ao carrinho'
+                imagem='carrinho.jpg'
+            />
+            <div className={styles.carrinho}>
+                {carrinho.map((item: ItemCarrinho) => <Item key={item.id} {...item} />)}
+                <div className={styles.total}>
+                    <strong>Resumo da compra</strong>
+                    <span>Subtotal: <strong>R$ 0,00</strong></span>
+                </div>
+            </div>
+        </>
+    )
+}
