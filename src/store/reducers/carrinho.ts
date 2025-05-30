b6a1c6cacf1c711a9c603c090a28470cffcdbaba
Melksedeque <freelancer@melksedeque.com.br>
2025-05-30 14:07:13 -0300

    Criação de arquivo de reducer para carrinho

---
+++ src/store/reducers/carrinho.ts
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const carrinhoSlice = createSlice({
+    name: 'carrinho',
+    initialState,
+    reducers: {},
+});
+
+export default carrinhoSlice.reducer;
