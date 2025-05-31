1c10f961427c75e3ad8571e1e090ccd7edd0d003
Melksedeque <freelancer@melksedeque.com.br>
2025-05-31 15:26:30 -0300

    Adição de condição para prop carrinho na interface de Item

--- src/types/Item.ts
+++ src/types/Item.ts
@@ -6,7 +6,7 @@ export interface Item {
     categoria: string;
     id: string;
     favorito: boolean;
-    carrinho: boolean;
+    carrinho?: boolean;
 }
 
 export interface ItemCarrinho extends Item {
