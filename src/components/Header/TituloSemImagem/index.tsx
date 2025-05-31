845d941e4c0a6b2f7d0aca3abd7c82f660da2c68
Melksedeque <freelancer@melksedeque.com.br>
2025-05-31 14:32:08 -0300

    Remoção de fragment do React, pois ele não era necessário

--- src/components/Header/TituloSemImagem/index.tsx
+++ src/components/Header/TituloSemImagem/index.tsx
@@ -3,11 +3,9 @@ import HeaderProps from "types/Header";
 
 export default function TituloSemImagem({titulo, descricao}: HeaderProps) {
     return (
-        <>
-            <div className={styles.container}>
-                <h1 className={styles.titulo}>{titulo}</h1>
-                <h2 className={styles.descricao}>{descricao}</h2>
-            </div>
-        </>
+        <div className={styles.container}>
+            <h1 className={styles.titulo}>{titulo}</h1>
+            <h2 className={styles.descricao}>{descricao}</h2>
+        </div>
     );
 }
