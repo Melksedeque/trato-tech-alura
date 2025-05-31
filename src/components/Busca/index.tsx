0c639126d1072b7b11f046d5f730d37b1d8baa9c
Melksedeque <freelancer@melksedeque.com.br>
2025-05-31 17:39:14 -0300

    Aplicação de dispatch de mudarBusca e restarBusca no componente

--- src/components/Busca/index.tsx
+++ src/components/Busca/index.tsx
@@ -1,12 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Busca.module.scss'
+import { mudarBusca, resetarBusca } from 'store/reducers/busca';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function Busca() {
+    const busca = useSelector(state => state.busca);
+    const dispatch = useDispatch();
+    const location = useLocation();
+
+    useEffect(() => {
+        dispatch(resetarBusca())
+    }, [location.pathname, dispatch]);
+
     return (
         <div className={styles.busca}>
             <input
                 type="text"
                 placeholder="O que você procura?"
                 className={styles.input}
+                value={busca}
+                onChange={e => dispatch(mudarBusca(e.target.value))}
             />
         </div>
     );
