9dd51ddc5db4cfee2aab2f2ea960b627cbfbd93b
Melksedeque <freelancer@melksedeque.com.br>
2025-05-31 15:01:09 -0300

    Importação de carrinhoSlice para puxar o persistance

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,7 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import categoriasSlice from './reducers/categorias'
 import itensSlice from './reducers/itens'
+import carrinhoSlice from './reducers/carrinho'
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; 
 
@@ -11,7 +12,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   categorias: categoriasSlice,
-  itens: itensSlice
+  itens: itensSlice,
+  carrinho: carrinhoSlice
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
