d308a54bb0a5c6e29a87ed8d3a924877eb0419f2
Melksedeque <freelancer@melksedeque.com.br>
2025-06-01 11:55:41 -0300

    Adequação do código do arquivo de rotas com Prettier

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,21 +1,21 @@
-import PaginaPadrao from "components/PaginaPadrao";
-import Categoria from "pages/Categoria";
-import Carrinho from "pages/Carrinho";
-import Home from "pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Anuncie from "pages/Anuncie";
+import PaginaPadrao from 'components/PaginaPadrao';
+import Categoria from 'pages/Categoria';
+import Carrinho from 'pages/Carrinho';
+import Home from 'pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Anuncie from 'pages/Anuncie';
 
 export default function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<PaginaPadrao />}>
-                    <Route index element={<Home />} />
-                    <Route path="/categoria/:nomeCategoria" element={<Categoria />} />
-                    <Route path="carrinho" element={<Carrinho />} />
-                    <Route path="anuncie" element={<Anuncie />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Home />} />
+          <Route path="/categoria/:nomeCategoria" element={<Categoria />} />
+          <Route path="carrinho" element={<Carrinho />} />
+          <Route path="anuncie" element={<Anuncie />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
