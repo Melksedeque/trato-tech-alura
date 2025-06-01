8d3c213a2eb7aff4c8c54712ed1f24816411e9c7
Melksedeque <freelancer@melksedeque.com.br>
2025-06-01 11:18:03 -0300

    Componente de página de anúncio

---
+++ src/pages/Anuncie/index.tsx
@@ -0,0 +1,10 @@
+import Header from 'components/Header'
+import styles from './Anuncie.module.scss'
+
+export default function Anuncie() {
+    return (
+        <div className={styles.anuncie}>
+            <Header titulo="Anunice aqui!" descricao="Anuncie seus produtos" />
+        </div>
+    )
+}
