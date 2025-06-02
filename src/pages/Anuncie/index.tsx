203ae65efce87bea37baaae6ca961125508992a0
Melksedeque <freelancer@melksedeque.com.br>
2025-06-02 10:51:12 -0300

    Formulário de Anunciar produtos

--- src/pages/Anuncie/index.tsx
+++ src/pages/Anuncie/index.tsx
@@ -1,10 +1,52 @@
 import Header from 'components/Header';
 import styles from './Anuncie.module.scss';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/index';
 
 export default function Anuncie() {
+  const categorias = useSelector((state: RootState) =>
+    state.categorias.map(({ nome, id }) => ({ nome, id }))
+  );
+
   return (
     <div className={styles.anuncie}>
-      <Header titulo="Anunice aqui!" descricao="Anuncie seus produtos" />
+      <Header
+        titulo="Anunice aqui!"
+        descricao="Anuncie seus produtos conosco"
+      />
+      <form action="">
+        <input
+          type="text"
+          placeholder="Nome do produto"
+          alt="Nome do Produto"
+        />
+        <input
+          type="text"
+          placeholder="Descrição do produto"
+          alt="Descrição do Produto"
+        />
+        <input
+          type="url"
+          placeholder="URL da imagem do produto"
+          alt="URL da imagem do Produto"
+        />
+        <select name="" id="">
+          <option value="" disabled selected>
+            -- Selecione a categoria --
+          </option>
+          {categorias.map(({ nome, id }) => (
+            <option key={id} value={id}>
+              {nome}
+            </option>
+          ))}
+        </select>
+        <input
+          type="number"
+          placeholder="Preço do produto"
+          alt="Preço do Produto"
+        />
+        <button type="submit">Cadastrar produto</button>
+      </form>
     </div>
   );
 }
