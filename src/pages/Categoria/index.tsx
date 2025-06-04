fcfa087fa82346308385520b4f96d914b26597d8
Melksedeque <freelancer@melksedeque.com.br>
2025-06-04 12:53:45 -0300

    Inclusão de rota de navegação para Anuncie com o nome da categoria

--- src/pages/Categoria/index.tsx
+++ src/pages/Categoria/index.tsx
@@ -4,8 +4,10 @@ import Header from 'components/Header';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Item from 'components/Item';
+import Botao from 'components/Botao';
 
 export default function Categoria() {
+  const navigate = useNavigate();
   const { nomeCategoria } = useParams();
   const { categoria, itens } = useSelector((state: RootState) => {
     const regexp = new RegExp(state.busca, 'i');
@@ -24,7 +26,14 @@ export default function Categoria() {
         titulo={categoria?.nome || ''}
         descricao={categoria?.descricao || ''}
         imagem={categoria?.header || ''}
-      />
+      >
+        <Botao
+          type="button"
+          onClick={() => navigate(`/anuncie/${nomeCategoria}`)}
+        >
+          Quero anunciar!
+        </Botao>
+      </Header>
       <div className={styles.itens}>
         {itens?.map((item) => <Item key={item.id} {...item} />)}
       </div>
