1b41cf80fc6f73acfedad1b220999c97c9d9217e
Melksedeque <freelancer@melksedeque.com.br>
2025-06-05 16:56:09 -0300

    Criação de atributo ref requisitado pelo forwardRef

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,22 @@
 import { Input as TypeInput } from 'types/Input';
 import styles from './Input.module.scss';
+import { forwardRef } from 'react';
 
-export default function Input({ type, name, value, onChange }: TypeInput) {
+function Input(
+  { type, name, value, placeholder, onChange }: TypeInput,
+  ref: React.Ref<HTMLInputElement>
+) {
   return (
     <input
+      ref={ref}
       type={type}
       name={name}
       value={value}
+      placeholder={placeholder}
       onChange={onChange}
       className={styles.input}
     />
   );
 }
+
+export default forwardRef(Input);
