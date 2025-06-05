2fa545d03187d2580834a9a972c3bf8de5d40bcd
Melksedeque <freelancer@melksedeque.com.br>
2025-06-05 16:41:10 -0300

    Desconstrução de props e importação de type

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,11 @@
 import { Input as TypeInput } from 'types/Input';
 import styles from './Input.module.scss';
 
-export default function Input({ type, value, onChange }: TypeInput) {
+export default function Input({ type, name, value, onChange }: TypeInput) {
   return (
     <input
       type={type}
+      name={name}
       value={value}
       onChange={onChange}
       className={styles.input}
