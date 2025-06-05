512502540d1f198f50907fb6bea297cb92ac4ca4
Melksedeque <freelancer@melksedeque.com.br>
2025-06-05 16:42:57 -0300

    Mais tipos possiveis para o Input

--- src/types/Input.ts
+++ src/types/Input.ts
@@ -2,5 +2,10 @@ export interface Input {
   type: 'text' | 'number' | 'url' | undefined;
   name?: string;
   value: string;
+  placeholder?: string;
+  min?: number;
+  max?: number;
+  disabled?: boolean;
+  required?: boolean;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
