cde14b94e770bb8930dc8eea272565aecc495a45
Melksedeque <freelancer@melksedeque.com.br>
2025-06-08 15:27:35 -0300

    Criação de services para buscar itens

--- src/services/itens.ts
+++ src/services/itens.ts
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { Item } from 'types/Item';
-import instance from 'common/config/api';
+import instance from 'config/api';
 
 // Thunk para buscar itens
 export const buscarItens = createAsyncThunk(
@@ -10,7 +10,7 @@ export const buscarItens = createAsyncThunk(
       const resposta = await instance.get<Item[]>('itens');
       return resposta.data;
     } catch (erro) {
-      return rejectWithValue('Erro ao buscar itens');
+      return rejectWithValue(`Erro ao buscar itens: ${erro}`);
     }
   }
 );
