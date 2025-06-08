70787590422b9dc88cedabe671b7c17d70885cdd
Melksedeque <freelancer@melksedeque.com.br>
2025-06-08 15:37:28 -0300

    Mudança nas páginas de Categorias para usar o Redux Thunk para consultar Categorias e seus respectivos Itens

--- src/pages/Categoria/index.tsx
+++ src/pages/Categoria/index.tsx
@@ -12,14 +12,22 @@ export default function Categoria() {
   const { categoria, itens } = useSelector((state: RootState) => {
     const regexp = new RegExp(state.busca, 'i');
     return {
-      categoria: state.categorias.find(
+      categoria: state.categorias.items.find(
         (categoria) => categoria.id === nomeCategoria
       ),
-      itens: state.itens.filter(
+      itens: state.itens.items.filter(
         (item) => item.categoria === nomeCategoria && item.titulo.match(regexp)
       ),
     };
   });
+
+  const { status: statusCategorias } = useSelector(
+    (state: RootState) => state.categorias
+  );
+  const { status: statusItens } = useSelector(
+    (state: RootState) => state.itens
+  );
+
   return (
     <div>
       <Header
@@ -35,6 +43,10 @@ export default function Categoria() {
         </Botao>
       </Header>
       <div className={styles.itens}>
+        {statusCategorias === 'loading' && <p>Carregando categorias...</p>}
+        {statusItens === 'loading' && <p>Carregando itens...</p>}
+        {statusCategorias === 'failed' && <p>Erro ao carregar categorias.</p>}
+        {statusItens === 'failed' && <p>Erro ao carregar itens.</p>}
         {itens?.map((item) => <Item key={item.id} {...item} />)}
       </div>
     </div>
