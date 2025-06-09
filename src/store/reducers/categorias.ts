9a40857f1f1cedaba76a6949e25a1451755ba617
Melksedeque <freelancer@melksedeque.com.br>
2025-06-09 17:18:46 -0300

    Restauração ao ponto anterior do Redux Thunk para correção e prática

--- src/store/reducers/categorias.ts
+++ src/store/reducers/categorias.ts
@@ -1,46 +1,15 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { Categoria } from 'types/Categoria';
-import { buscarCategorias } from 'services/categorias';
-
-interface CategoriasState {
-  items: Categoria[];
-  status: 'idle' | 'loading' | 'succeeded' | 'failed';
-  error: string | null;
-}
-
-const initialState: CategoriasState = {
-  items: [],
-  status: 'idle',
-  error: null,
-};
 
 const categoriasSlice = createSlice({
   name: 'categorias',
-  initialState,
+  initialState: [] as Categoria[],
   reducers: {
-    adicionarCategorias: (state, { payload }: PayloadAction<Categoria[]>) => {
-      state.status = 'succeeded';
-      state.items = payload;
+    adicionarCategorias: (_state, { payload }) => {
+      return payload;
     },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(buscarCategorias.pending, (state) => {
-        state.status = 'loading';
-        state.error = null;
-      })
-      .addCase(buscarCategorias.fulfilled, (state, { payload }) => {
-        state.status = 'succeeded';
-        state.items = payload;
-      })
-      .addCase(buscarCategorias.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error =
-          (action.payload as string) || 'Ocorreu um erro desconhecido';
-      });
   },
 });
 
 export const { adicionarCategorias } = categoriasSlice.actions;
-
 export default categoriasSlice.reducer;
