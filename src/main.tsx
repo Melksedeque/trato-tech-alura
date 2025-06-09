c0ff924ef778da851e2ba7521d13da87119c1e48
Melksedeque <freelancer@melksedeque.com.br>
2025-06-09 09:31:13 -0300

    Criação de Toast para mensagens e alertas

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,24 @@ import Router from './routes';
 import store, { persistor } from './store/index';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { createStandaloneToast } from '@chakra-ui/toast';
+
+const { ToastContainer, toast } = createStandaloneToast();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Router />
+        <ToastContainer />
       </PersistGate>
     </Provider>
   </StrictMode>
 );
+
+toast({
+  title: 'Teste de Toast',
+  description: 'Está funcionando o Toast',
+  status: 'success',
+  duration: 3000,
+});
